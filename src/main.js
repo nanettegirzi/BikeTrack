a40417ef66fe60e71d3f99d1451d5f889939d97e
Nanette Girzi <ngirzi@gmail.com>
2018-03-22 15:55:26 -0700

    add more styling

--- src/main.js
+++ src/main.js
@@ -26,16 +26,12 @@ $(document).ready(function() {
       for( let i = 0; i < response.bikes.length; i++)
       {
 
-        $("#output").append("<ul>" + `<img src=${response.bikes[i].thumb} onerror="this.src='https://image.flaticon.com/icons/svg/13/13698.svg';">` + "<h3>" + response.bikes[i].title + "</h3>" + "<br>" + "<h4>" + "<strong> Color: </strong> " + response.bikes[i].frame_colors + "</h4>" + "<br>" + "<h4>" + "<strong> Date stolen: </strong>" + timeConverter(response.bikes[i].date_stolen) + "</h4>" + "</ul>" + "<hr>");
+        $("#output").append(`<img src=${response.bikes[i].thumb} onerror="this.src='https://image.flaticon.com/icons/svg/13/13698.svg';">` + "<h3>" + response.bikes[i].title + "</h3>"  + "<p>" + "<strong>Color: </strong> " + response.bikes[i].frame_colors + "</p>" + "<p>" + "<span id='infoHeaders'> Date stolen: </span>" + timeConverter(response.bikes[i].date_stolen) + "</p>" + "<strong>Location: </strong>" + response.bikes[i].stolen_location + "<hr>");
       }
     }).fail(function(error) {
       $("#showErrors").text("There was an error in processing your request: ${error.responseText}. Please try again!")
     });
 
-    ("#resetButton").click(function(){
-      ("#citySearch").val("");
-      ("#distanceSearch").val("");
-    })
 
   });
 });
